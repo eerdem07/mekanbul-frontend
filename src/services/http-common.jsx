e14dca5880dd6419c4f765353fd2836c988f719c
Emre Erdem <emreerdem502@gmail.com>
2024-12-28 21:23:50 +0300

    bug fixed

--- src/services/http-common.jsx
+++ src/services/http-common.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const apiBaseURL =
-  "https://mekan-bul-backend-gjsv4uyrw-jacobss7s-projects.vercel.app";
+  "http://mekan-bul-backend-gjsv4uyrw-jacobss7s-projects.vercel.app";
 
 export default axios.create({
-  baseURL: apiBaseURL,
+  baseURL: apiBaseURL + "/api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
